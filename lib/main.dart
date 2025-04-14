1bf49f4a1b778ce91fa9ec51ab7b18b2b52385a8
dev-raj <vasoyaraj2617@gmail.com>
2025-04-14 11:43:34 -1200

    add school /news screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'constant/export.dart';
-import 'service/firebase_options.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+void main()  {
   runApp(const MyApp());
 }
 
